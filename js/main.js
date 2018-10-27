94ba8f8f5ae2bb634b76b131c78f95fd7de49a88
Demola John <29120743+ceofvo@users.noreply.github.com>
2018-10-27 16:42:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = altText;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -177,7 +177,7 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
